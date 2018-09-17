3bb4ec40fe3362c22e77a79c4e03e748cc131142
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-09-17 11:23:42 +0900

    タイトル変更

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="App-title">Welcome to My App</div>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
