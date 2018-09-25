f50079e72f1870309e499e5019a7bc0fc448e42d
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-09-25 14:53:58 +0900

    button.js追加

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hello from './hello';
 import Profile from './profile';
 import List from './list';
 import Switch from './switch';
+import Button from './button';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <Profile name="Hanako" age="10" height="140" weight="35" love="Cats" />
         <List data={listData} />
         <Switch />
+        <Button />
       </div>
     );
   }
---
+++ src/button.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Button extends Component {
+
+    constructor() {
+        super();
+    }
+
+    handleClick() {
+        alert('button click!!');
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>BUTTON</button>
+            </div>
+        );
+    }
+}
+
+export default Button;
