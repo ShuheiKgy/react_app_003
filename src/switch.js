5f01ba95b73e94603ac6e9116c5033332c967965
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-09-25 15:01:14 +0900

    switch.js修正

--- src/switch.js
+++ src/switch.js
@@ -4,16 +4,27 @@ class Switch extends Component {
 
     constructor() {
         super();
+
+        this.handleClick = this.handleClick.bind(this);
         this.state = {
             power: false
         };
     }
 
+    handleClick() {
+        this.setState({
+            power: !this.state.power
+        })
+    }
+
     render() {
         return (
-            <p>
-                {this.state.power ? 'ON' : 'OFF' }
-            </p>
+            <div>
+                <p>
+                    {this.state.power ? 'ON' : 'OFF' }
+                </p>
+                <button onClick={this.handleClick}>SWITCH</button>
+            </div>
         );
     }
 }
