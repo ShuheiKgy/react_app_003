5d4703684acef2e2699b09224a69b5ab7a1468e8
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-09-25 14:34:31 +0900

    switch追加

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Hello from './hello';
 import Profile from './profile';
 import List from './list';
+import Switch from './switch';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Profile name="Taro" age="20" love="Game" />
         <Profile name="Hanako" age="10" love="Cats" />
         <List data={listData} />
+        <Switch />
       </div>
     );
   }
---
+++ src/switch.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Switch extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            power: false
+        };
+    }
+
+    render() {
+        return (
+            <p>
+                {this.state.power ? 'ON' : 'OFF' }
+            </p>
+        );
+    }
+}
+
+export default Switch;
