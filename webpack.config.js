57f5570d5d1c25fb7cd43a8bdac3e4a40e14e381
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-10-06 11:08:55 +0900

    HtmlWebpackPlugin追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     watch: true,
@@ -9,21 +10,27 @@ module.exports = {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options:  {
-                        presets: ['env', 'react']
-                    }
-                },
-                exclude: /node_modules/
-            }
-        ]
-    },
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.js$/,
+    //             use: {
+    //                 loader: 'babel-loader',
+    //                 options:  {
+    //                     presets: ['env', 'react']
+    //                 }
+    //             },
+    //             exclude: /node_modules/
+    //         }
+    //     ]
+    // },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin()
+        // new webpack.optimize.UglifyJsPlugin({
+        //     extractComments: true
+        // }),
+        new HtmlWebpackPlugin({
+            title: 'Plugin generate page',
+            template: 'src/root.html'
+        })
     ]
 }
