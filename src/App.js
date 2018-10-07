95e52c5fb8b2ad4fb8bb987619c8fd091f1cce70
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-10-07 21:42:16 +0900

    App.js修正

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React, {Component} from 'react';
-import Hello from './hello';
-import Profile from './profile';
-import List from './list';
-import Switch from './switch';
-import Button from './button';
+import SwitchFlag from './switchflag';
+import ViewFlagValue from './viewflagvalue';
 
 class App extends Component {
-  render() {
-    const listData = ['foo', 'bar', 'baz'];
+  constructor() {
+    super();
 
-    return (
+    this.state = {
+      flag: false
+    }
+  }
+
+  render() {
+    return(
       <div>
-        <Hello name="Taro" />
-        <Profile name="Taro" age="20" height="170" weight="65" love="Game" />
-        <Profile name="Hanako" age="10" height="140" weight="35" love="Cats" />
-        <List data={listData} />
-        <Switch />
-        <Button />
+        <SwitchFlag />
+        <ViewFlagValue flag={this.state.flag} />
       </div>
     );
   }
