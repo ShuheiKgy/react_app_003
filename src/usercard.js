b330d82c9689771fbf14ea4c056494bbeacbcdfb
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-10-07 21:21:16 +0900

    usercard.jsをSFCで実装

---
+++ src/usercard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const UserCard = (props) => {
+    return (
+        <div>
+            <p>{props.name}</p>
+            <p>{props.age}</p>
+        </div>
+    );
+}
+
+
+// import React, {Component} from 'react';
+
+// class UserCard extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <p>{this.props.title}</p>
+//                 <p>{this.props.age}</p>
+//             </div>
+//         );
+//     }
+// }
+
+export default UserCard;
