22201c9f596f6633c4b834584d6ac2957740affe
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-10-09 17:38:34 +0900

    buildが通るように修正

--- webpack.config.js
+++ webpack.config.js
@@ -5,39 +5,47 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     watch: true,
-    entry: './src/index.js',
+    entry: [
+        'babel-polyfill',
+        'react-hot-loader/patch',
+        './src/index.js',
+    ],
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/'
     },
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         port: 9000,
         hot: true,
     },
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.js$/,
-    //             use: {
-    //                 loader: 'babel-loader',
-    //                 options:  {
-    //                     presets: ['env', 'react']
-    //                 }
-    //             },
-    //             exclude: /node_modules/
-    //         }
-    //     ]
-    // },
-    plugins: [
-        // new webpack.optimize.UglifyJsPlugin({
-        //     extractComments: true
-        // }),
-        new HtmlWebpackPlugin({
-            title: 'Plugin generate page',
-            template: 'src/root.html'
-        }),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-    ]
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
+                loader: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    // plugins: [
+    //     // new webpack.optimize.UglifyJsPlugin({
+    //     //     extractComments: true
+    //     // }),
+    //     // new HtmlWebpackPlugin({
+    //     //     title: 'Plugin generate page',
+    //     //     template: 'src/root.html'
+    //     // }),
+    //     // new webpack.NamedModulesPlugin(),
+    //     // new webpack.HotModuleReplacementPlugin(),
+    // ]
 }
