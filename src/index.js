64737be0611de0f177e6d8bdd24e5c73030e6f5d
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-10-12 23:20:54 +0900

    index.js修正

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-// import Module from './module';
-
-// const root = document.querySelector('#root');
-// root.innerHTML = Module;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
